9d2e6074c2cf427edc61453f637a95bc44cf8adc
Vladimir Muratov <vladimirmuratov@mail.ru>
2021-03-16 19:04:55 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter} from "react-router-dom"
+
+import Header from 'components/header'
+import Content from 'components/content'
+
+
+const App = () => {
+    return (
+        <div>
+            <Header/>
+            <Content/>
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <App/>
+        </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
+);
